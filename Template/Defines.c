db47833334cdc1710b2323d7b7982321c7394fa5
Fischer <76223353+FischerTG@users.noreply.github.com>
2021-03-11 15:56:02 +0100

    Create Defines.c

---
+++ Template/Defines.c
@@ -0,0 +1,12 @@
+/*-----Masks for single switch read-----*/
+#define MASK_S0 0x01
+#define MASK_S1 0x02
+#define MASK_S2 0x04
+#define MASK_S3 0x08
+#define MASK_S4 0x10
+#define MASK_S5 0x20
+#define MASK_S6 0x40
+#define MASK_S7 0x80
+
+/*-----Mask for PORT read---------------*/
+#define MASK 0x01
