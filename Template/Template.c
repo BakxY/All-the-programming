ff684dc6d5daee4da883b7b517dd99b28861b1e7
Fischer <76223353+FischerTG@users.noreply.github.com>
2021-03-11 15:49:38 +0100

    Create Template.c

---
+++ Template/Template.c
@@ -0,0 +1,32 @@
+/*-----------------------------------
+* Project:  "Project Title"			*
+* Autor:    "Your Name"				*
+* Date:     "Date of commit"		*
+* Dep:      b4 microcontrollers		*
+* uC:       STM32F107VC				*
+------------------------------------*/
+
+/*----------Libraries---------------*/
+#include <stm32f10x.h>								//include header file for uC
+#include "TouchP0P1.h"								//include header file for display
+/*----------Defines-----------------*/
+//found in GIT defines
+#define true 1
+#define false 0
+/*----------Variables---------------*/
+//local variables are prefered
+
+/*----------User functions----------*/
+//found in GIT user functions folder
+
+/*----------User code---------------*/
+int main(void) 										//main function
+{
+	
+	InitTouchP0P1("1r");								//Display initialisation r -> rotated display
+	
+	while (1)
+	{
+		//write epic 10/10 code here
+	}
+}
