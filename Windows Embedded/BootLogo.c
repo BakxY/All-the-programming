5808cb11244362dfad559073f65c93d44248c2b3
Fischer <76223353+FischerTG@users.noreply.github.com>
2021-03-12 20:33:37 +0100

    Create BootLogo.c

---
+++ Windows Embedded/BootLogo.c
@@ -0,0 +1,97 @@
+void BootScreen(void)
+{
+	int iCount = 0;
+	int ix, iy;
+	/*-----------RED SQUARE------------*/
+	ix = 35;
+	iy = 42;
+	for(iCount = 0; iCount < 7; iCount++)
+	{
+		ix += 5;
+		iy -= 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_RED, 1);
+	}
+	for(iCount = 0; iCount < 5; iCount++)
+	{
+		ix += 5;
+		iy += 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_RED, 1);
+	}
+	/*----------GREEN SQUARE-----------*/
+	ix = 99;
+	iy = 40;
+	for(iCount = 0; iCount < 6; iCount++)
+	{
+		ix += 5;
+		iy += 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_GREEN, 1);
+	}
+	for(iCount = 0; iCount < 6; iCount++)
+	{
+		ix += 5;
+		iy -= 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_GREEN, 1);
+	}
+	/*-----------BLUE SQUARE-----------*/
+	ix = 35;
+	iy = 107;
+	for(iCount = 0; iCount < 7; iCount++)
+	{
+		ix += 5;
+		iy -= 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_BLUE, 1);
+	}
+	for(iCount = 0; iCount < 5; iCount++)
+	{
+		ix += 5;
+		iy += 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_BLUE, 1);
+	}
+	/*----------YELLOW SQUARE----------*/
+	ix = 99;
+	iy = 105;
+	for(iCount = 0; iCount < 6; iCount++)
+	{
+		ix += 5;
+		iy += 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_YELLOW, 1);
+	}
+	for(iCount = 0; iCount < 6; iCount++)
+	{
+		ix += 5;
+		iy -= 2;
+		rectan(ix, iy, (ix+4), (iy+60), 1, BRIGHT_YELLOW, 1);
+	}
+	/*----------WINDOWS TEXT-----------*/
+	textxy("Windows", 40, 185, WHITE, BLACK);
+	textxy("Embedded", 40, 198, WHITE, BLACK);
+	/*----------COPYRIGHT TEXT---------*/
+	textxy("(c) Fischer & Brexit 2021", 0, 315, WHITE, BLACK);
+	/*----------LOADING CIRCLE---------*/
+	textxy("Windows is updating", 40, 235, WHITE, BLACK);
+	textxy("Please dont shut off", 40, 250, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o                10%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o o              21%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o o o            38%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o o o o          51%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o o o o o        69%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o o o o o o      88%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	textxy("o o o o o o o    99%", 40, 265, WHITE, BLACK);
+	delayWorky(0xB00000); 
+	textxy("o o o o o o o o 101%", 40, 265, WHITE, BLACK);
+	delayWorky(0xA00000);
+	for(iCount = 0; iCount < 3; iCount++)
+	{
+		textxy("SUCCESS !!!", 40, 280, WHITE, BLACK);
+		delayWorky(0x800000);
+		textxy("           ", 40, 280, WHITE, BLACK);
+		delayWorky(0x800000);
+	}
+}
