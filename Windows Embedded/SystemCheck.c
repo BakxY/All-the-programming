7bee7866b957be734d63c0af62d132f66bfc3689
Fischer <76223353+FischerTG@users.noreply.github.com>
2021-03-12 21:51:49 +0100

    Create SystemCheck.c

---
+++ Windows Embedded/SystemCheck.c
@@ -0,0 +1,63 @@
+void SystemCheck(void)
+{
+	int iCount;
+	int iMCount = 1;
+	
+	printAt(0, "System check started");
+	printAt(1, "--------------------");
+	
+	while(iCount < 10)
+	{
+		printAt(3, "/");
+		delayWorky(0x100000);
+		printAt(3, "-");
+		delayWorky(0x100000);
+		printAt(3, "\\");
+		delayWorky(0x100000);
+		printAt(3, "|");
+		delayWorky(0x100000);
+		
+		iMCount = iCount;
+		while(iMCount > 0)
+		{
+			print(" o");
+			iMCount--;
+		}
+		iCount++;
+		switch(iCount)
+		{
+			case 2:
+				printAt(5, "uController: STM32F107VC");
+				break;
+			case 3:
+				printAt(6, "uC Initialisation OK");
+				break;
+			case 4:
+				printAt(7, "LCD Initialisation OK");
+				break;
+			case 5:
+				printAt(8, "Systemclock 12MHZ OK");
+				break;
+			case 6:
+				printAt(9, "RAM Check OK");
+				break;
+			case 7:
+				printAt(10, "Driver updated OK");
+				break;
+			case 8:
+				printAt(11, "OS System updated OK");
+				break;
+			case 9:
+				printAt(12, "Securing from NDB");
+				break;
+			case 10:
+				printAt(15, "All OK starting OS");
+				delayWorky(0xF00000);
+				clearScreen(BLACK);
+				break;
+			default:
+				textxy("SystemCheck.c (V1)", 0, 315, WHITE, BLACK);
+				break;
+		}
+	}
+}
