38f6d642a3583ba3df6344d7313d0f0de55bb4f5
Severin Sprenger <79839223+BakxY@users.noreply.github.com>
2021-03-12 12:24:56 +0100

    Added

--- Template/User_Functions.c
+++ Template/User_Functions.c
@@ -21,6 +21,28 @@ int* ReadPort(void)									//function for reading port input
 	}
 	return iPinArray;								//return the port in array !!! needs to be safed in int pointer -> int* iPinArray; -> iPinArray = ReadPort(); !!!
 }
+/*-----Function for setting full PORT from array-----*/
+void OutputPort(int iStatePort[])
+{
+  int iMaskP[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};  //array for masks which are used to set the port to 1
+  int iMaskN[] = {0xFE ,0xFD ,0xFB ,0xF7 ,0xEF ,0xDF ,0xBF ,0x7F};  //array for masks which are used to set the port to 0
+  int i;                              //placeholder variable for the for loops
+  
+  for(i = 0; i <= 8; i++)             //for loop which is used to cycle the masks 
+  {
+    if(iStatePort[i] == 1)            //compare wanted state of pin with 1
+    {
+      P1 |= iMaskP[i];                //set single pin with the positiv mask
+    }
+  }
+  for(i = 0; i <= 8; i++)             //for loop which is used to cycle the masks
+  {
+    if(iStatePort[i] == 0)            //compare wanted state of pin with 0
+    {
+      P1 &= iMaskN[i];                //set single pin with the negativ mask
+    }
+  }
+}
 //use in main
 int main(void)
 {
