e847948cd90b71c475c915bded4b5d31171d3bda
Severin Sprenger <79839223+BakxY@users.noreply.github.com>
2021-03-13 01:05:29 +0100

    Created bash v3

--- Windows Embedded/Bash.c
+++ Windows Embedded/Bash.c
@@ -1,9 +1,10 @@
-unsigned int x = 8; //These need to be global variables
-unsigned int y = 1;
+//these variables need to be global
+int iOpfer = 0; 											//Opfervariable for evading coruption on array xy
+int yx[2] = {1, 0};
 
 void Bash(void)
 {
-  textxy("BakxY_Bash_V2", 0, 315, WHITE, BLACK);
+  textxy("BakxY_Bash_V3", 0, 315, WHITE, BLACK);
   
   int iInput;
   int iStatus[8];
@@ -14,28 +15,31 @@ void Bash(void)
   iStatus[1] = !((1 >> (iInput & 0x40)) == (1 >= 0x40)); //Down
   iStatus[2] = !((1 >> (iInput & 0x08)) == (1 >> 0x08)); //Right
   iStatus[3] = !((1 >> (iInput & 0x20)) == (1 >= 0x20)); //Left
-  
-  rectan(x, y, x+8, y+14, 2, BRIGHT_RED, 0);
-  
+	
   /*-------Setting x and y----------*/
-  if(iStatus[0] == 1 && y != 1)
-  {
-    y -= 16;
-    delayWorky(0x3FFF00);
-  }
-  else if(iStatus[1] == 1 && y <= 280)
-  {
-    y += 16;
-    delayWorky(0x3FFF00);
-  }
-  else if(iStatus[2] == 1 && x <= 230)
-  {
-    x += 8;
-    delayWorky(0x3F0000);
-  } 
-  else if(iStatus[3] == 1 && x >= 8)
-  {
-    x -= 8;
-    delayWorky(0x3F0000);
-  }
+	if((iStatus[0] == 1) || (iStatus[1] == 1) || (iStatus[2] == 1) || (iStatus[3] == 1))
+	{
+		rectan(yx[1], yx[0], yx[1]+8, yx[0]+14, 2, BLACK, 0);
+		if(iStatus[0] == 1 && yx[0] != 1)
+		{
+			yx[0] -= 16;
+			delayWorky(0x3FFF00);
+		}
+		else if(iStatus[1] == 1 && yx[0] <= 280)
+		{
+			yx[0] += 16;
+			delayWorky(0x3FFF00);
+		}
+		else if(iStatus[2] == 1 && yx[1] <= 230)
+		{
+			yx[1] += 8;
+			delayWorky(0x3F0000);
+		} 
+		else if(iStatus[3] == 1 && yx[1] >= 8)
+		{
+			yx[1] -= 8;
+			delayWorky(0x3F0000);
+		}
+		rectan(yx[1], yx[0], yx[1]+8, yx[0]+14, 2, BRIGHT_RED, 0);
+	}
 }
