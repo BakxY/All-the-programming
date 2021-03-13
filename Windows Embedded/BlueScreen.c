c86959ecc587db98ff20f1e0c0cbb91d8a12d3ce
Fischer <76223353+FischerTG@users.noreply.github.com>
2021-03-13 14:22:06 +0100

    Update BlueScreen.c

--- Windows Embedded/BlueScreen.c
+++ Windows Embedded/BlueScreen.c
@@ -1,24 +1,24 @@
-void BlueScreen(void)
-{
+void BlueScreen(int iErrorCode)
+{	
 	clearScreen(BRIGHT_BLUE);
 	
 	/*-----------VERSION TEXT----------*/
-	textxy("FischerTG_BlueScreen_V2", 0, 315, WHITE, BRIGHT_BLUE);
+	textxy("FischerTG_BlueScreen_V3", 0, 315, WHITE, BRIGHT_BLUE);
 	/*------------BSoD TEXT------------*/
-	textxy("    #       ##  ###     ## ", 10, 30, WHITE, BRIGHT_BLUE);
+	textxy("    #       ##  ###     ##", 10, 30, WHITE, BRIGHT_BLUE);
 	textxy("o  #        # # #       # #", 10, 42, WHITE, BRIGHT_BLUE);
 	textxy("   #        ##  ### ### # #", 10, 54, WHITE, BRIGHT_BLUE);
 	textxy("o  #        # #   # # # # #", 10, 66, WHITE, BRIGHT_BLUE);
-	textxy("    #       ##  ### ### ## ", 10, 78, WHITE, BRIGHT_BLUE);
+	textxy("    #       ##  ### ### ##", 10, 78, WHITE, BRIGHT_BLUE);
 	/*-------------RESTART-------------*/
 	textxy("your uController has ran in", 10, 110, WHITE, BRIGHT_BLUE);
 	textxy("a problem and needs restart", 10, 125, WHITE, BRIGHT_BLUE);
-	delayWorky(0xA00000);
+	delayWorky(0x971B00);
 	/*----------ERROR TEXT-------------*/
 	textxy("an exception has occurred at", 10, 165, WHITE, BRIGHT_BLUE);
 	textxy("0x07FA'884A OVERFLOW ERROR", 10, 180, WHITE, BRIGHT_BLUE);
 	textxy("triggered by: infinite loop", 10, 195, WHITE, BRIGHT_BLUE);
-	delayWorky(0xA00000);
+	delayWorky(0x971B00);
 	/*----------ERROR CODE-------------*/
 	if(iErrorCode == 1)
 	{
@@ -28,6 +28,6 @@ void BlueScreen(void)
 	{
 		textxy("error-code: 404 not found", 10, 250, WHITE, BRIGHT_BLUE);	
 	}
-	delayWorky(0xF00000);
+	delayWorky(0xEE3600);
 	clearScreen(BLACK);
 }
